5e39d413509cfab074d06bd66f5b0f4f80bc7e0f
Charith Ellawala <charithe@users.noreply.github.com>
2024-01-26 09:03:36 +0000

    Update deps (#62)
    Signed-off-by: Charith Ellawala <charith@cerbos.dev>

--- cerbos/start.sh
+++ cerbos/start.sh
@@ -1,4 +1,9 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 docker run -i -t -p 3592:3592 -p 3593:3593 \
-  -v $(pwd)/policies:/policies \
-  ghcr.io/cerbos/cerbos:latest \
-  server
+	-v "${SCRIPT_DIR}/policies:/policies" \
+	ghcr.io/cerbos/cerbos:latest \
+	server
